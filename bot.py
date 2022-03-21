c34ef46d5958300aad2d6543c782886c9809c34d
DM880 <denys.mauro@gmail.com>
2022-03-21 07:46:58 +0000

    url validator added , comments added

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup as BS
 from selenium import webdriver
+import validators
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+# set up Chrome
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -13,13 +17,23 @@ s=Service(ChromeDriverManager().install())
 
 browser = webdriver.Chrome(service=s, options=chrome_options)
 
+# Prompt user for url
+
 url =  input('Enter url: ')
+
+# Check if url is valid
+
+if not validators.url(url):
+    while not validators.url(url):
+        url =  input('Enter valid url: ')
+
+# Start scraping
 
 browser.get(url)
 
 soup = BS(browser.page_source,features="html.parser")
 
-video = soup.find_all(['iframe','img','video'],{"src":True})
+video = soup.find_all(['iframe','img','video','div'],{"src":True})
 
 video_url = []
 
