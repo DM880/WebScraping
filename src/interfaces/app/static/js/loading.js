89f38c599a279beba6bc994d767ccdefe9bf6ea3
DM880 <denys.mauro@gmail.com>
2022-04-29 10:12:46 +0000

    updates

---
+++ src/interfaces/app/static/js/loading.js
@@ -0,0 +1,11 @@
+function loading(){
+    $(window).ready(function() {
+        $('#body').hide();
+        $('#loading').show();
+    });
+    }
+
+$(window).ready(function() {
+    $('#body').show();
+    $('#loading').hide();
+});
